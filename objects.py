5ef3b955453084ebe4d03d7adc09be4fa12c5d5e
Hirafathima <hirafathima125@gmail.com>
2020-10-14 23:02:21 +0530

    authority_mapping

---
+++ objects.py
@@ -0,0 +1,45 @@
+link = [
+    {'basic':
+        {
+            'Primary School':['general education department-education.kerala.gov.in'],
+            'Public toilet': ['Suchitva Mission-http://sanitation.kerala.gov.in/community-and-public-toilets/ '],
+            'Streetlight':['kerala public works department-www.keralapwd.gov.in','Kerala state electricity board limited-www.kseb.in'],
+            'Health clinic': ['Health and family welfare department of Kerala - https://kerala.gov.in/health-family-welfare-department','National Rural Health Mission - http://arogyakeralam.gov.in/'],
+            'Municipal water supply':['kerala water authority office - kwa.kerala.gov.in','Kerala water authority water supply division office-'],
+            'Road reconstruction':['Kerala Public works department- www.keralapwd.gov.in'],
+            'Avoid power cuts':['Kerala state electricity board limited-www.kseb.in'],
+            'Waste management':['Kerala waste management authority - sanitation.kerala.gov.in'],
+            'Housing':['kerala state housing board-www.kshb.kerala.gov.in'],
+            'Public transportation':['Kerala State Road Transport Corporation - www.keralartc.com','National Transportation Planning and Research Centre - www.natpac.kerala.gov.in'],
+            'Social security' :['social justice department,kerala-swd.kerala.gov.in'],
+            'Ration': ['Civil Supplies Department(Corporation) KERALA-https://www.civilsupplieskerala.gov.in']
+        }
+    },
+    {'standard':
+        {
+            'Public Library':['State central library-https://statelibrary.kerala.gov.in/','Higher education department of Kerala-http://www.highereducation.kerala.gov.in/'],
+            'Taxi service': [''],
+            'Public park & playgrounds':[''],
+            'Traffic control': ['Road safety authority-https://roadsafety.kerala.gov.in/'],
+            'Night patrol':['Kerala police-keralpolice.gov.in'],
+            'Bus waiting shed':['Kerala public works department-www.keralapwd.gov.in'],
+            'Better disaster prevention schemes':['Kerala state disaster management authority-sdma.kerala.gov.in'],
+            'Railways':['Kerala Rail Development Corporation Limited (KRDCL)-www.indianrail.gov.in','Southern Railway website-www.sr.indianrailways.gov.in'],
+            'National highways':['Kerala Public Works Department. - https://www.pwd.kerala.gov.in/NHwing','National Highway Authority of India-https://www.nhai.gov.in/'],
+            'Bridges':['Roads and Bridges Development Corporation of Kerala Ltd (RBDCK)-www.rbdck.com'],
+            'Police stations' :['kerala police-keralpolice.gov.in']
+        }
+    },
+    {'premium':
+        {
+            'Gym':['Panchayat-'],
+            'Theatre': ['Kerala State Film Development Corporation-http://www.ksfdc.in/'],
+            'Tourist Resorts':['kerala tourism department-www.keralatourism.org'],
+            'Metro rail services': ['Kerala Public Works Department-https://www.pwd.kerala.gov.in'],
+            'Free wifi':['keralaIT-www.keralait.org'],
+            'Museums and zoos':['Kerala State Department of Museums and Zoos-http://www.museumandzoo.kerala.gov.in/'],
+            'IT parks':['kerala IT department-www.keralait.org/it-parks'],
+            'Turf':['']
+        }
+    }
+]
