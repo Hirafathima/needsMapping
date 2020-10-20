05ddfca20805300862804d304b421362455a9226
Hirafathima <hirafathima125@gmail.com>
2020-10-20 14:41:46 +0530

    encrypted

--- app.py
+++ app.py
@@ -22,12 +22,28 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+
+with open("config.py","r") as outfile:
+    config = _import_('config')
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('serviceaccount.json')
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "needsmapping",
+  "private_key_id": config.private_key_id,
+  "private_key": config.private_key,
+  "client_email": config.client_email,
+  "client_id": config.client_id,
+  "auth_uri": config.auth_uri,
+  "token_uri": config.token_uri,
+  "auth_provider_x509_cert_url": config.auth_provider_x509_cert_url,
+  "client_x509_cert_url": config.client_x509_cert_url
+})
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://needsmapping.firebaseio.com/'
     })
+
+
 
 # def update_m():
     
